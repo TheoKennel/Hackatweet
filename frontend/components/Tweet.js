b6de7847b508dd1ebd44d1d513745fb29ceb3ef6
ClementTANGUY <clemdelatang@gmail.com>
2023-04-27 17:02:32 +0200

    Add Tweet css

--- frontend/components/Tweet.js
+++ frontend/components/Tweet.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { removeTweet } from '../reducers/tweets';
@@ -8,7 +9,10 @@ import styles from '../styles/Tweet.module.css';
 function Tweet(props) {
 
   const dispatch = useDispatch();
-  const user = useSelector((state) => state.user.value);
+
+  const timeOut = Date.now - props.date
+
+  const message = props.message;
 
   const handleTrashTweet = () => {
     dispatch(removeTask(props));
@@ -17,11 +21,11 @@ function Tweet(props) {
   return (
     <div className={styles.tweet}>
       <div className={styles.tweetHeader}>
-        <Image src="/egg.jpg" alt="twitterEgg" width={50} height={50} className={styles.logoImg}/>
-        <span>{user.firstname} @{user.username} . postedTime</span>
+        <Image src="/egg.jpeg" alt="twitterEgg" width={50} height={50} className={styles.avatar}/>
+        <span className={styles.userDetails}>{props.firstname} @{props.username} . {timeOut} </span>
       </div>
       <div className={styles.message}>
-        <p>Mesaage with hashtags</p>
+        {message}
       </div>
       <div className={styles.tweetIcons}>
       <FontAwesomeIcon icon={faHeart} className={styles.like} onClick={()=> handleLikeTweet()}/>
