9824080e06ca9e2a327dce65299aac3ab315e820
Théo <theokennel@gmail.com>
2023-04-28 11:00:24 +0200

    Route du backend Ok, modification du model

--- backend/models/tweets.js
+++ backend/models/tweets.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
+
+
 const tweetSchema = new mongoose.Schema({
   username: String,
   firstname: String,
   date: { type: Date, default: Date.now },
   content: String,
-  hashtags: [{ type: String }],
+  hashtags: [String],
 });
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
--- backend/routes/tweet.js
+++ backend/routes/tweet.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var router = express.Router();
 require('../models/connection');
-const Tweet = require('../models/tweets');
+const Tweet = require('../models/tweets')
+const { checkBody } = require('../modules/checkBody');
 
 // Route pour créer un nouveau tweet
 router.post('/', (req, res) => {
-  const { username, firstname, content} = req.body;
+  const { content } = req.body;
+  const { username, firstname } = req.body; // Supposons que vous récupérez username et firstname du frontend via req.user
 
+  // Vérifier que le champ `content` est présent
+  if (!checkBody(req.body, ['content'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  // Utiliser une expression régulière pour extraire les hashtags du contenu
   const hashtags = content.match(/#\w+/g) || [];
 
   const newTweet = new Tweet({
@@ -16,9 +25,10 @@ router.post('/', (req, res) => {
     hashtags,
   });
 
-  newTweet.save()
-    .then((tweet) => {
-      res.status(201).json({ result: true, tweet });
+  newTweet
+    .save()
+    .then((data) => {
+      res.status(201).json({ data });
     })
 });
 
@@ -30,18 +40,46 @@ router.get('/', (req, res) => {
     })
 });
 
-router.get('/', (req, res) => {
-    Tweet.find()
-      .then((tweets) => {
-        const hashtags = tweets.reduce((acc, tweet) => {
-          // Concaténer tous les hashtags extraits de chaque tweet
-          return acc.concat(tweet.hashtags);
-        }, []);
+//Route pour supprimer un tweet
+router.delete('/:tweetId', (req,res) => {
+  const { tweetId } = req.params
   
-        // Supprimer les doublons des hashtags
-        const uniqueHashtags = [...new Set(hashtags)];
-        res.status(200).json({ result: true, hashtags: uniqueHashtags });
-      })
-  });
+  Tweet.deleteOne({ _id : tweetId }) 
+  .then((data) => {
+    if(data.deletedCount > 0) {
+    res.json({result: true})
+  } else {
+    res.json({ result: false})
+  }
+  })
+})
 
+// Récupère tous les hashtags
+router.get('/hashtag', (req, res) => {
+  Tweet.find()
+    .then((tweets) => {
+      const hashtags = tweets.reduce((acc, tweet) => {
+        // Concaténer tous les hashtags extraits de chaque tweet
+        return acc.concat(tweet.hashtags);
+      }, []);
+
+      // Supprimer les doublons des hashtags
+      const uniqueHashtags = [...new Set(hashtags)];
+      res.status(200).json({ result: true, hashtags: uniqueHashtags });
+    })
+});
+
+
+// Route pour récupérer les tweets par hashtag
+router.get('/hashtag/:hashtag', (req, res) => {
+  const { hashtag } = req.params;
+
+  Tweet.find({ hashtags: hashtag })
+    .then((tweets) => {
+      console.log(tweets.content);
+      res.json({ result: true, tweets });
+    })
+});
+
+module.exports = router;
 module.exports = router;
