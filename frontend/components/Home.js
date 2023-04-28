6e1611b7d126a5507e1c7d6b7f2ee290c9424147
ClementTANGUY <clemdelatang@gmail.com>
2023-04-28 14:41:31 +0200

    Changes in home

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -12,6 +12,49 @@ function Home() {
   const router = useRouter();
   const dispatch = useDispatch();
 
+  const [tweetsData, setTweetsData] = useState([]);
+
+  const likedTweetsData = useSelector((state) => state.likedTweets.value);
+
+  // Get all tweets in DB
+  useEffect(() => {
+    fetch('http://localhost:3000/tweet')
+      .then(response => response.json())
+      .then(data => {
+        setTweetsData(data);
+      });
+  }, []);
+
+  // Like tweet
+  const handleLikeTweet = () => {
+    if (isLiked) {
+			dispatch(removeLikedTweet(tweet));
+		} else {
+			dispatch(addLikedTweet(tweet));
+		}
+  };
+
+
+
+  const tweets = tweetsData;
+  if (tweets.length > 0) {
+    tweets.sort((a,b)=> a.date === b.date ? 0 : a.date ? -1 : 1);
+    tweets.map((tweet, i) => {
+      const isLiked = likedTweetsData.some(likedTweet => likedTweet.date === tweet.date);
+      const timeOut = Math.floor((Date.now() - tweet.date) / 1000);
+      if (timeOut < 60) {
+
+      } else if (timeOut >=60 && timeOut < 3600) {
+
+      } else if (timeOut >= 3600 && timeOut < 86400 ) {
+
+      } else {
+
+      }
+      <LastTweets key={i} isLiked={isLiked} tweet={...tweet} />;
+    });
+  }
+
   const handleLogout = () => {
     dispatch(logout());
     router.push('/'); // Redirection vers la page de connexion après la déconnexion
--- frontend/components/LastTweets.js
+++ frontend/components/LastTweets.js
@@ -7,73 +7,32 @@ import styles from '../styles/LastTweets.module.css';
 
 function LastTweets(props) {
 
-  const [tweetsData, setTweetsData] = useState([]);
-
-  const likedTweetsData = useSelector((state) => state.likedTweets.value);
-  const dispatch = useDispatch();
-
-  // Get all tweets in DB
-  useEffect(() => {
-    fetch('http://localhost:3000/tweet')
-      .then(response => response.json())
-      .then(data => {
-        setTweetsData(data);
-      });
-  }, []);
-
-  // Like tweet
-  const handleLikeTweet = (tweet) => {
-    const isLiked = likedTweetsData.some(likedTweet => likedTweet.date === tweet.date);
-    if (isLiked) {
-			dispatch(removeLikedTweet(tweet));
-		} else {
-			dispatch(addLikedTweet(tweet));
-		}
-  };
-
   // Delete a tweet in DB
   const handleTrashTweet = () => {
 
   }
 
-  const tweets = tweetsData;
-  if (tweets.length > 0) {
-    tweets.sort((a,b)=> a.date === b.date ? 0 : a.date ? -1 : 1);
-    tweets.map((tweet, i) => {
-
-      const timeOut = Math.floor((Date.now() - tweet.date) / 1000);
-      if (timeOut < 60) {
-
-      } else if (timeOut >=60 && timeOut < 3600) {
-
-      } else if (timeOut >= 3600 && timeOut < 86400 ) {
-
-      } else {
-
-      }
-
-      let iconStyle = {};
-      if (isLiked) {
-        iconStyle = { 'color': 'red' };
-      }
-
-      return (
-        <div className={styles.tweet}>
-          <div className={styles.tweetHeader}>
-            <Image src="/egg.jpeg" alt="twitterEgg" width={50} height={50} className={styles.avatar}/>
-            <span className={styles.userDetails}>{tweet.firstname} @{tweet.username} .  </span>
-          </div>
-          <div className={styles.message}>
-            {tweet.content}
-          </div>
-          <div className={styles.tweetIcons}>
-          <FontAwesomeIcon icon={faHeart} className={styles.like} style={iconStyle} onClick={()=> handleLikeTweet(tweet)}/>
-          <FontAwesomeIcon icon={faTrash} className={styles.delete} onClick={()=> handleTrashTweet()}/>
-          </div>
-        </div>
-      );
-    });
+  let iconStyle = {};
+  if (isLiked) {
+      iconStyle = { 'color': 'red' };
   }
+
+  return (
+    <div className={styles.tweet}>
+      <div className={styles.tweetHeader}>
+        <Image src="/egg.jpeg" alt="twitterEgg" width={50} height={50} className={styles.avatar}/>
+        <span className={styles.userDetails}>{tweet.firstname} @{tweet.username} .  </span>
+      </div>
+      <div className={styles.message}>
+        {tweet.content}
+      </div>
+      <div className={styles.tweetIcons}>
+        <FontAwesomeIcon icon={faHeart} className={styles.like} style={iconStyle} onClick={()=> handleLikeTweet(tweet)}/>
+        <FontAwesomeIcon icon={faTrash} className={styles.delete} onClick={()=> handleTrashTweet()}/>
+      </div>
+    </div>
+  );
+
 }
 
 export default LastTweets;
